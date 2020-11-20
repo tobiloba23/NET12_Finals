caddd1995a04b6708ee6e7d9cff9cbb09f7c1fa0
Tobi <tobi.adesanya@wirepick.com>
2020-11-19 19:47:37 -0500

    Completed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person[] people = new Person[]{
+                new Adult(),
+                new Child()
+            };
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"First Name: {person.FirstName}, Last Name: {person.LastName}");
+            }
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        public class Adult : Person
+        {
+            public Adult() {
+                FirstName = "AdultFirstName";
+                LastName = "AdultLastName";
+            }
+        }
+
+        public class Child : Person
+        {
+            public Child()
+            {
+                FirstName = "ChildFirstName";
+                LastName = "ChildLastName";
+            }
         }
     }
 }
